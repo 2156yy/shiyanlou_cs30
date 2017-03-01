c5b6a2e39765a269d98ec91276da8ff592b487bf
2156yy <yy734526503@qq.com>
2017-03-01 17:58:02 +0800

    time

--- django/work/mysite/mysite/urls.py
+++ django/work/mysite/mysite/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # Examples:	
 	url(r'^hello/$',hello),
 	url(r'^time/$', current_datetime),
+	url(r'^time/plus/\d{1,2}/$', hours_ahead),
 )
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
--- django/work/mysite/mysite/views.py
+++ django/work/mysite/mysite/views.py
@@ -1,7 +1,7 @@
 
 
 import datetime
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 
 #def hello(request):
 #	return HttpResponse("Hello world")
@@ -9,4 +9,14 @@ from django.http import HttpResponse
 def current_datetime(request):
 	now = datetime.datetime.now()
 	html = "<html><body>It is now %s.</body></html>" % now
+	return HttpResponse(html)
+
+
+def hours_ahead(request,offset):
+	try:
+		offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now() + datetime.timedelte(hours=offset)
+	htmk = "<html><body>In %s hour(s), it will be %s .</body><>/html" %(offsetdt)
 	return HttpResponse(html)
